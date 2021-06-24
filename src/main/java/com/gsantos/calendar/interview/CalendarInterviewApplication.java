7fb6696d3a56f33d49b69ec03a68acf5578ba6b5
Gustavo Santos <gustavo.santos@waitrose.co.uk>
2021-06-24 17:08:54 +0100

    Setup project

---
+++ src/main/java/com/gsantos/calendar/interview/CalendarInterviewApplication.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021.
+ * Author: Gustavo Santos.
+ */
+
+package com.gsantos.calendar.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsantos/calendar/interview/CalendarInterviewApplicationTests.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021.
+ * Author: Gustavo Santos.
+ */
+
+package com.gsantos.calendar.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
